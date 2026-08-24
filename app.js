f522d681157a227930d4d6e1e1a2fca583aebdb2
aryandarafryn1234-byte <aryandarafryn1234@gmail.com>
2026-08-25 02:58:58 +0330

    Refactor game initialization and navigation logic

--- app.js
+++ app.js
@@ -1,130 +1,19 @@
-let tg = window.Telegram.WebApp;
+function openGame(game){
 
-tg.ready();
-tg.expand();
+if(game=="rocket"){
 
-
-let board=[
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""
-];
-
-
-let player="❌";
-let score=1000;
-
-
-function draw(){
-
-let html="";
-
-for(let i=0;i<9;i++){
-
-html+=`
-<div class="cell" onclick="play(${i})">
-${board[i]}
-</div>
-`;
-
-}
-
-document.getElementById("board").innerHTML=html;
+window.location.href=
+"rocket/index.html";
 
 }
 
 
-function play(i){
+if(game=="xo"){
 
-if(board[i]!="") return;
-
-
-board[i]=player;
-
-
-if(check()){
-
-alert(player+" برنده شد 🎉");
-
-score+=100;
-
-document.getElementById("score").innerHTML=score;
-
-return;
+window.location.href=
+"xo/index.html";
 
 }
 
 
-player =
-player=="❌"
-?"⭕"
-:"❌";
-
-
-draw();
-
 }
-
-
-function check(){
-
-let win=[
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
-];
-
-
-for(let x of win){
-
-if(
-board[x[0]] &&
-board[x[0]]==board[x[1]] &&
-board[x[1]]==board[x[2]]
-){
-
-return true;
-
-}
-
-}
-
-return false;
-
-}
-
-
-
-function restart(){
-
-board=[
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""
-];
-
-player="❌";
-
-draw();
-
-}
-
-
-draw();
