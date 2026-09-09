199f5326b440273700173c35f91a37973421cc24
aryandarafryn1234-byte <aryandarafryn1234@gmail.com>
2026-09-10 02:50:53 +0330

    Add UsersController to AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [],
+  controllers: [UsersController],
   providers: [PrismaService],
   exports: [PrismaService],
 })
