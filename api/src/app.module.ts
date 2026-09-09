1638840c2313062c84c7b8f62bf3e68d527b257c
aryandarafryn1234-byte <aryandarafryn1234@gmail.com>
2026-09-10 03:01:51 +0330

    Add GamesController and DailyBonusController to AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersController } from './users.controller';
+import { GamesController } from './games.controller';
+import { DailyBonusController } from './daily-bonus.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  controllers: [
+    UsersController,
+    GamesController,
+    DailyBonusController,
+  ],
+  providers: [
+    PrismaService,
+  ],
+  exports: [
+    PrismaService,
+  ],
 })
 export class AppModule {}
